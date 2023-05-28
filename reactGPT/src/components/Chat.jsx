9c0a75419c49f15c5fb5a03fbd548d4bf6c01227
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-27 23:52:37 -0400

    correct system message handling, no streaming

--- reactGPT/src/components/Chat.jsx
+++ reactGPT/src/components/Chat.jsx
@@ -1,69 +1,75 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import './Chat.css';
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./Chat.css";
 
 Chat.propTypes = {
-    modelName: PropTypes.string,
-    systemMessage: PropTypes.string,
-  };
+  modelName: PropTypes.string,
+  systemMessage: PropTypes.string,
+};
 
 function Chat(props) {
   const [messages, setMessages] = useState([]);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-  
+
     let userInput = event.target.user_input.value.trim();
-  
+
+    // Add user message to messages
+    let newMessages = [...messages, { role: "user", content: userInput }];
+
     // Update system message
-    if (props.systemMessage && (!messages.some(message => message.role === 'system') || props.systemMessage !== messages.find(message => message.role === 'system').content)) {
-      let systemMessageIndex = messages.findIndex((message) => message.role === 'system');
+    if (props.systemMessage) {
+      let systemMessageIndex = newMessages.findIndex(
+        (message) => message.role === "system"
+      );
       // If the system message exists in array, remove it
       if (systemMessageIndex !== -1) {
-        messages.splice(systemMessageIndex, 1);
+        newMessages.splice(systemMessageIndex, 1);
       }
-      messages.push({ role: 'system', content: props.systemMessage });
+      newMessages.push({ role: "system", content: props.systemMessage });
     }
-  
-    // Add user message to messages
-    messages.push({ role: 'user', content: userInput });
-    setMessages([...messages]);
-  
+
     // Send request to server
     const response = await fetch("http://localhost:8000/gpt4", {
       method: "POST",
       body: JSON.stringify({
-        messages: messages,
+        messages: newMessages,
         model_type: props.modelName,
       }),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    
+
+    setMessages([...messages, { role: "user", content: userInput }]);
+
     // Handle server response
     if (response.ok) {
       const reader = response.body.getReader();
       const decoder = new TextDecoder("utf-8");
       let assistantMessage = "";
-  
+
       // eslint-disable-next-line no-constant-condition
       while (true) {
         const { value, done } = await reader.read();
         if (done) {
-          setMessages(prevMessages => [...prevMessages, { role: 'assistant', content: assistantMessage }]);
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            { role: "assistant", content: assistantMessage },
+          ]);
           break;
         }
-  
+
         const text = decoder.decode(value);
         assistantMessage += text;
       }
     } else {
       console.error(`Error: ${response.status}`);
     }
-  
+
     // Clear user input
-    event.target.user_input.value = '';
+    event.target.user_input.value = "";
   };
 
   return (
@@ -72,7 +78,11 @@ function Chat(props) {
       <div className="chat-wrapper">
         <div id="chat-messages" className="chat-box">
           {messages.map((message, index) => (
-            <div key={index} className={`message ${message.role === 'user' ? 'user-message' : 'assistant-message'}`}>
+            <div
+              key={index}
+              className={`message ${
+                message.role === "user" ? "user-message" : "assistant-message"
+              }`}>
               <p>{message.content}</p>
             </div>
           ))}
@@ -85,7 +95,9 @@ function Chat(props) {
             name="user_input"
             placeholder="Type your message..."
           />
-          <button className="button" type="submit" id="submitBtn">Send</button>
+          <button className="button" type="submit" id="submitBtn">
+            Send
+          </button>
         </form>
       </div>
     </div>
