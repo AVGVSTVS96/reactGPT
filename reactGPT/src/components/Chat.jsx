2f0dc2a4a42211b5427840f1baaa9757f71adaa1
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-29 11:49:41 -0400

    streaming working but words doubled

--- reactGPT/src/components/Chat.jsx
+++ reactGPT/src/components/Chat.jsx
@@ -3,145 +3,173 @@ import { useState } from "react";
 import "./Chat.css";
 
 Chat.propTypes = {
-  modelName: PropTypes.string,
-  systemMessage: PropTypes.string,
+    modelName: PropTypes.string,
+    systemMessage: PropTypes.string,
 };
 
 function Chat(props) {
-  const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState([]);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    let userInput = event.target.user_input.value.trim();
+        let userInput = event.target.user_input.value.trim();
 
-    // Add user message to messages
-    let newMessages = [...messages, { role: "user", content: userInput }];
+        // Add user message to messages
+        let newMessages = [...messages, { role: "user", content: userInput }];
 
-    // Update system message
-    if (props.systemMessage) {
-      let systemMessageIndex = newMessages.findIndex(
-        (message) => message.role === "system"
-      );
-      // If the system message exists in array, remove it
-      if (systemMessageIndex !== -1) {
-        newMessages.splice(systemMessageIndex, 1);
-      }
-      newMessages.push({ role: "system", content: props.systemMessage });
-    }
+        // Update system message
+        if (props.systemMessage) {
+            let systemMessageIndex = newMessages.findIndex(
+                (message) => message.role === "system"
+            );
+            // If the system message exists in array, remove it
+            if (systemMessageIndex !== -1) {
+                newMessages.splice(systemMessageIndex, 1);
+            }
+            newMessages.push({ role: "system", content: props.systemMessage });
+        }
 
-    // Send request to server
-    const response = await fetch("http://localhost:8000/gpt4", {
-      method: "POST",
-      body: JSON.stringify({
-        messages: newMessages,
-        model_type: props.modelName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+        // Send request to server
+        const response = await fetch("http://localhost:8000/gpt4", {
+            method: "POST",
+            body: JSON.stringify({
+                messages: newMessages,
+                model_type: props.modelName,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-    setMessages([...messages, { role: "user", content: userInput }]);
-
-
+        setMessages([...messages, { role: "user", content: userInput }]);
 
 
-    // no chunk streaming
-    // if (response.ok) {
-    //   const reader = response.body.getReader();
-    //   const decoder = new TextDecoder("utf-8");
-    //   let assistantMessage = "";
+        // no chunk streaming
+        // if (response.ok) {
+        //   const reader = response.body.getReader();
+        //   const decoder = new TextDecoder("utf-8");
+        //   let assistantMessage = "";
 
-    //   // eslint-disable-next-line no-constant-condition
-    //   while (true) {
-    //     const { value, done } = await reader.read();
-    //     if (done) {
-    //       setMessages((prevMessages) => [
-    //         ...prevMessages,
-    //         { role: "assistant", content: assistantMessage },
-    //       ]);
-    //       break;
-    //     }
+        //   // eslint-disable-next-line no-constant-condition
+        //   while (true) {
+        //     const { value, done } = await reader.read();
+        //     if (done) {
+        //       setMessages((prevMessages) => [
+        //         ...prevMessages,
+        //         { role: "assistant", content: assistantMessage },
+        //       ]);
+        //       break;
+        //     }
 
-    //     const text = decoder.decode(value);
-    //     assistantMessage += text;
-    //   }
-    // } else {
-    //   console.error(`Error: ${response.status}`);
-    // }
+        //     const text = decoder.decode(value);
+        //     assistantMessage += text;
+        //   }
+        // } else {
+        //   console.error(`Error: ${response.status}`);
+        // }
 
-//   // Stream chunks as different message divs
-//   if (response.ok) {
-//     const reader = response.body.getReader();
-//     const decoder = new TextDecoder("utf-8");
+        //   // Stream chunks as different message divs
+        //   if (response.ok) {
+        //     const reader = response.body.getReader();
+        //     const decoder = new TextDecoder("utf-8");
 
-//     while (true) {
-//       const { value, done } = await reader.read();
-//       if (done) break;
+        //     while (true) {
+        //       const { value, done } = await reader.read();
+        //       if (done) break;
 
-//       const text = decoder.decode(value);
-//       setMessages(prevMessages => [...prevMessages, { role: 'assistant', content: text }]);
-//     }
-//   } else {
-//     console.error(`Error: ${response.status}`);
-//   }
+        //       const text = decoder.decode(value);
+        //       setMessages(prevMessages => [...prevMessages, { role: 'assistant', content: text }]);
+        //     }
+        //   } else {
+        //     console.error(`Error: ${response.status}`);
+        //   }
 
-  // Handle server response
-  // Attempt to stream chunks as one message div
-  if (response.ok) {
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder("utf-8");
+        // Handle server response
+        // Attempt to stream chunks as one message div
 
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) break;
+        // Sherief's code
+        //   if (response.ok) {
+        //     const reader = response.body.getReader();
+        //     const decoder = new TextDecoder("utf-8");
 
-      const text = decoder.decode(value);
-      setMessages(prevMessages => {
-        let lastMessage = prevMessages.pop();
-        lastMessage = [{ role: "assistant", content: assistantMessage }]
-      })
-      
-    }
-  } else {
-    console.error(`Error: ${response.status}`);
-  }
+        //     while (true) {
+        //       const { value, done } = await reader.read();
+        //       if (done) break;
 
-    // Clear user input
-    event.target.user_input.value = "";
-  };
+        //       const text = decoder.decode(value);
+        //       setMessages(prevMessages => {
+        //         let lastMessage = prevMessages.pop();
+        //         lastMessage = [{ role: "assistant", content: assistantMessage }]
+        //       })
 
-  return (
-    <div className="chat-container">
-      <h1 className="heading">Canvas GPT</h1>
-      <div className="chat-wrapper">
-        <div id="chat-messages" className="chat-box">
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`message ${
-                message.role === "user" ? "user-message" : "assistant-message"
-              }`}>
-              <p>{message.content}</p>
+        //     }
+        //   } else {
+        //     console.error(`Error: ${response.status}`);
+        //   }
+
+
+        if (response.ok) {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder("utf-8");
+
+            // eslint-disable-next-line no-constant-condition
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) break;
+
+                const text = decoder.decode(value);
+                setMessages(prevMessages => {
+                    // Check if last message was from the assistant
+                    if (prevMessages.length > 0 && prevMessages[prevMessages.length - 1].role === "assistant") {
+                        // Append text to the last assistant message
+                        let lastMessage = prevMessages[prevMessages.length - 1];
+                        lastMessage.content += text;
+                        return [...prevMessages]; // Return the messages including the updated last message
+                    } else {
+                        // If last message was not from the assistant, add a new message from the assistant
+                        return [...prevMessages, { role: "assistant", content: text }];
+                    }
+                });
+            }
+        } else {
+            console.error(`Error: ${response.status}`);
+        }
+
+
+        // Clear user input
+        event.target.user_input.value = "";
+    };
+
+    return (
+        <div className="chat-container">
+            <h1 className="heading">Canvas GPT</h1>
+            <div className="chat-wrapper">
+                <div id="chat-messages" className="chat-box">
+                    {messages.map((message, index) => (
+                        <div
+                            key={index}
+                            className={`message ${message.role === "user" ? "user-message" : "assistant-message"
+                                }`}>
+                            <p>{message.content}</p>
+                        </div>
+                    ))}
+                </div>
+                <form id="chat-form" className="input-form" onSubmit={handleFormSubmit}>
+                    <textarea
+                        type="text"
+                        className="input"
+                        id="user-input"
+                        name="user_input"
+                        placeholder="Type your message..."
+                    />
+                    <button className="button" type="submit" id="submitBtn">
+                        Send
+                    </button>
+                </form>
             </div>
-          ))}
         </div>
-        <form id="chat-form" className="input-form" onSubmit={handleFormSubmit}>
-          <textarea
-            type="text"
-            className="input"
-            id="user-input"
-            name="user_input"
-            placeholder="Type your message..."
-          />
-          <button className="button" type="submit" id="submitBtn">
-            Send
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Chat;
