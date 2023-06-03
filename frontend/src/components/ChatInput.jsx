eed8c279abf5a3a0a48a7de26f956e979115db8c
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-06-02 23:59:48 -0400

    move input styles into their own file previously input styling was split between `chat.css` and `menubar.css`

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import './styles/Input.css';
+
 
 ChatInput.propTypes = {
   onSubmit: PropTypes.func.isRequired,
--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import './styles/MenuBar.css';
+import './styles/Input.css';
+
 
 Menubar.propTypes = {
   modelName: PropTypes.string,
